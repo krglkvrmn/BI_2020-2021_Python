c9301910e1c75b60f42ff979266f3e75af41dbc6
Kruglikov Roman <kruglikov1911@mail.ru>
2020-09-09 01:38:47 +0300

    Removed 'eval, unnecessary code ommited.

--- calculator.py
+++ calculator.py
@@ -1,27 +1,33 @@
-from math import floor
-
-
 while True:
     try:
         a = float(input())
         operator = input()
         assert operator in ('+', '-', '/', '*', '**', '//', '%')
         b = float(input())
-        expression = f'{a}{operator}{b}'
-        float_res = eval(expression)
+        if operator == '+':
+            res = a + b
+        elif operator == '+':
+            res = a - b
+        elif operator == '/':
+            res = a / b
+        elif operator == '*':
+            res = a * b
+        elif operator == '**':
+            res = a ** b
+        elif operator == '//':
+            res = a // b
+        elif operator == '%':
+            res = a % b
         # Return integer value if possible. For example: 5.0 -> 5
-        result = int(float_res) if floor(float_res) == float_res else float_res
+        result = int(res) if (res % 1 == 0) else res
+        print(result)
 
     # Start again if invalid operator is entered.
     except AssertionError:
         print('Invalid operator. Try again.')
-        continue
     # Start again if division by zero was performed.
     except ZeroDivisionError:
         print('Zero division! Try again.')
-        continue
     # Start again if operands are not numbers.
     except ValueError:
         print('Operands must be numbers. Try again.')
-        continue
-    print(result)
