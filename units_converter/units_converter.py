afbd93ad3492d357f11feaf4cf48a1cc9608ce08
Kruglikov Roman <kruglikov1911@mail.ru>
2021-03-15 00:05:37 +0300

    second homework isolated

--- units_converter/units_converter.py
+++ units_converter/units_converter.py
@@ -23,7 +23,6 @@ HELP_MESSAGE = """
                   adding new units to units.json"""
 
 
-
 def print_available_units(units: dict):
     for unit_type in units:
         print(f'{unit_type}: {" ".join(units[unit_type])}')
